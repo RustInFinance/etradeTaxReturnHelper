323988512cdd47b9ae25c7927ef621e92fddbf8e
Jacek Czaja <jacek.czaja@intel.com>
2025-12-14 20:32:07 +0100

    clap update (#169)
    * - clap updated
    
    * - Bumped up clap
    
    * Remove not needed dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: 2022-2025 RustInFinance
 // SPDX-License-Identifier: BSD-3-Clause
 
-use clap::{App, AppSettings, Arg};
+use clap::{Arg, Command};
 use std::env;
 
 mod de;
@@ -29,20 +29,19 @@ use logging::ResultExt;
 // TODO: Drag&Drop to work on MultiBrowser field
 // TODO: taxation of EUR instruments in US
 
-fn create_cmd_line_pattern<'a, 'b>(myapp: App<'a, 'b>) -> App<'a, 'b> {
+fn create_cmd_line_pattern(myapp: Command) -> Command {
     myapp
         .arg(
-            Arg::with_name("residency")
+            Arg::new("residency")
                 .long("residency")
                 .help("Country of residence e.g. pl , us ...")
                 .value_name("FILE")
-                .takes_value(true)
                 .default_value("pl"),
         )
         .arg(
-            Arg::with_name("financial documents")
+            Arg::new("financial documents")
                 .help("Brokerage statement PDFs  and Gain & Losses xlsx documents\n\nBrokerege statements can be downloaded from:\n\thttps://edoc.etrade.com/e/t/onlinedocs/docsearch?doc_type=stmt\n\nGain&Losses documents can be downloaded from:\n\thttps://us.etrade.com/etx/sp/stockplan#/myAccount/gainsLosses\n")
-                .multiple(true)
+                .num_args(1..)
                 .required(true),
         )
 }
@@ -62,14 +61,15 @@ fn main() {
         }
     }
 
-    let myapp = App::new("etradeTaxHelper ".to_string() + VERSION)
-        .setting(AppSettings::ArgRequiredElseHelp);
+    let myapp = Command::new("etradeTaxHelper")
+        .version(VERSION)
+        .arg_required_else_help(true);
     let matches = create_cmd_line_pattern(myapp).get_matches_from(wild::args());
 
     let residency = matches
-        .value_of("residency")
+        .get_one::<String>("residency")
         .expect_and_log("error getting residency value");
-    let rd: Box<dyn etradeTaxReturnHelper::Residency> = match residency {
+    let rd: Box<dyn etradeTaxReturnHelper::Residency> = match residency.as_str() {
         "de" => Box::new(de::DE {}),
         "pl" => Box::new(pl::PL {}),
         "us" => Box::new(us::US {}),
@@ -80,7 +80,7 @@ fn main() {
     };
 
     let pdfnames = matches
-        .values_of("financial documents")
+        .get_many::<String>("financial documents")
         .expect_and_log("error getting brokarage statements pdfs names.\n\nBrokerege statements can be downloaded from:\n\nhttps://edoc.etrade.com/e/t/onlinedocs/docsearch?doc_type=stmt\n\n");
 
     let pdfnames: Vec<String> = pdfnames.map(|x| x.to_string()).collect();
@@ -107,7 +107,7 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use clap::{App, ErrorKind};
+    use clap::Command;
 
     #[test]
     fn test_exchange_rate_de() -> Result<(), String> {
@@ -193,24 +193,22 @@ mod tests {
     #[test]
     fn test_cmdline_de() -> Result<(), clap::Error> {
         // Init Transactions
-        let myapp = App::new("E-trade tax helper");
-        let matches = create_cmd_line_pattern(myapp).get_matches_from_safe(vec![
+        let myapp = Command::new("E-trade tax helper");
+        let matches = create_cmd_line_pattern(myapp).get_matches_from(vec![
             "mytest",
             "--residency=de",
             "data/example.pdf",
-        ])?;
-        let residency = matches.value_of("residency").ok_or(clap::Error {
-            message: "Unable to get residency value".to_owned(),
-            kind: ErrorKind::InvalidValue,
-            info: None,
-        })?;
-        match residency {
+        ]);
+        let residency = matches
+            .get_one::<String>("residency")
+            .ok_or(clap::error::Error::new(
+                clap::error::ErrorKind::InvalidValue,
+            ))?;
+        match residency.as_str() {
             "de" => return Ok(()),
-            _ => clap::Error {
-                message: "Wrong residency value".to_owned(),
-                kind: ErrorKind::InvalidValue,
-                info: None,
-            },
+            _ => clap::error::Error::<clap::error::DefaultFormatter>::new(
+                clap::error::ErrorKind::InvalidValue,
+            ),
         };
         Ok(())
     }
@@ -218,56 +216,52 @@ mod tests {
     #[test]
     fn test_cmdline_pl() -> Result<(), clap::Error> {
         // Init Transactions
-        let myapp = App::new("E-trade tax helper");
-        let matches = create_cmd_line_pattern(myapp).get_matches_from_safe(vec![
+        let myapp = Command::new("E-trade tax helper");
+        let matches = create_cmd_line_pattern(myapp).get_matches_from(vec![
             "mytest",
             "--residency=pl",
             "data/example.pdf",
-        ])?;
-        let residency = matches.value_of("residency").ok_or(clap::Error {
-            message: "Unable to get residency value".to_owned(),
-            kind: ErrorKind::InvalidValue,
-            info: None,
-        })?;
-        match residency {
+        ]);
+        let residency = matches
+            .get_one::<String>("residency")
+            .ok_or(clap::error::Error::new(
+                clap::error::ErrorKind::InvalidValue,
+            ))?;
+        match residency.as_str() {
             "pl" => return Ok(()),
-            _ => clap::Error {
-                message: "Wrong residency value".to_owned(),
-                kind: ErrorKind::InvalidValue,
-                info: None,
-            },
+            _ => clap::error::Error::<clap::error::DefaultFormatter>::new(
+                clap::error::ErrorKind::InvalidValue,
+            ),
         };
         Ok(())
     }
     #[test]
     fn test_cmdline_default() -> Result<(), clap::Error> {
         // Init Transactions
-        let myapp = App::new("E-trade tax helper");
-        create_cmd_line_pattern(myapp).get_matches_from_safe(vec!["mytest", "data/example.pdf"])?;
+        let myapp = Command::new("E-trade tax helper");
+        create_cmd_line_pattern(myapp).get_matches_from(vec!["mytest", "data/example.pdf"]);
         Ok(())
     }
 
     #[test]
     fn test_cmdline_us() -> Result<(), clap::Error> {
         // Init Transactions
-        let myapp = App::new("E-trade tax helper");
-        let matches = create_cmd_line_pattern(myapp).get_matches_from_safe(vec![
+        let myapp = Command::new("E-trade tax helper");
+        let matches = create_cmd_line_pattern(myapp).get_matches_from(vec![
             "mytest",
             "--residency=us",
             "data/example.pdf",
-        ])?;
-        let residency = matches.value_of("residency").ok_or(clap::Error {
-            message: "Unable to get residency value".to_owned(),
-            kind: ErrorKind::InvalidValue,
-            info: None,
-        })?;
-        match residency {
+        ]);
+        let residency = matches
+            .get_one::<String>("residency")
+            .ok_or(clap::error::Error::new(
+                clap::error::ErrorKind::InvalidValue,
+            ))?;
+        match residency.as_str() {
             "us" => return Ok(()),
-            _ => clap::Error {
-                message: "Wrong residency value".to_owned(),
-                kind: ErrorKind::InvalidValue,
-                info: None,
-            },
+            _ => clap::error::Error::<clap::error::DefaultFormatter>::new(
+                clap::error::ErrorKind::InvalidValue,
+            ),
         };
         Ok(())
     }
@@ -275,14 +269,16 @@ mod tests {
     #[test]
     fn test_unrecognized_file_taxation() -> Result<(), clap::Error> {
         // Get all brokerage with dividends only
-        let myapp = App::new("E-trade tax helper").setting(AppSettings::ArgRequiredElseHelp);
+
+        let myapp = Command::new("etradeTaxHelper").arg_required_else_help(true);
+
         let rd: Box<dyn etradeTaxReturnHelper::Residency> = Box::new(pl::PL {});
         // Check printed values or returned values?
         let matches = create_cmd_line_pattern(myapp)
-            .get_matches_from_safe(vec!["mytest", "unrecognized_file.txt"])?;
+            .get_matches_from(vec!["mytest", "unrecognized_file.txt"]);
 
         let pdfnames = matches
-            .values_of("financial documents")
+            .get_many::<String>("financial documents")
             .expect_and_log("error getting financial documents names");
         let pdfnames: Vec<String> = pdfnames.map(|x| x.to_string()).collect();
 
@@ -296,10 +292,10 @@ mod tests {
     #[ignore]
     fn test_dividends_taxation() -> Result<(), clap::Error> {
         // Get all brokerage with dividends only
-        let myapp = App::new("E-trade tax helper").setting(AppSettings::ArgRequiredElseHelp);
+        let myapp = Command::new("etradeTaxHelper").arg_required_else_help(true);
         let rd: Box<dyn etradeTaxReturnHelper::Residency> = Box::new(pl::PL {});
         // Check printed values or returned values?
-        let matches = create_cmd_line_pattern(myapp).get_matches_from_safe(vec![
+        let matches = create_cmd_line_pattern(myapp).get_matches_from(vec![
             "mytest",
             "data/Brokerage Statement - XXXX0848 - 202202.pdf",
             "data/Brokerage Statement - XXXX0848 - 202203.pdf",
@@ -310,10 +306,10 @@ mod tests {
             "data/Brokerage Statement - XXXX0848 - 202211.pdf",
             "data/Brokerage Statement - XXXX0848 - 202212.pdf",
             "data/G&L_Collapsed.xlsx",
-        ])?;
+        ]);
 
         let pdfnames = matches
-            .values_of("financial documents")
+            .get_many::<String>("financial documents")
             .expect_and_log("error getting brokarage statements pdfs names");
         let pdfnames: Vec<String> = pdfnames.map(|x| x.to_string()).collect();
 
@@ -339,18 +335,18 @@ mod tests {
     #[ignore]
     fn test_sold_dividends_taxation() -> Result<(), clap::Error> {
         // Get all brokerage with dividends only
-        let myapp = App::new("E-trade tax helper").setting(AppSettings::ArgRequiredElseHelp);
+        let myapp = Command::new("etradeTaxHelper").arg_required_else_help(true);
         let rd: Box<dyn etradeTaxReturnHelper::Residency> = Box::new(pl::PL {});
-        let matches = create_cmd_line_pattern(myapp).get_matches_from_safe(vec![
+        let matches = create_cmd_line_pattern(myapp).get_matches_from(vec![
             "mytest",
             "data/Brokerage Statement - XXXX0848 - 202202.pdf",
             "data/Brokerage Statement - XXXX0848 - 202203.pdf",
             "data/Brokerage Statement - XXXX0848 - 202204.pdf",
             "data/Brokerage Statement - XXXX0848 - 202205.pdf",
             "data/G&L_Collapsed.xlsx",
-        ])?;
+        ]);
         let pdfnames = matches
-            .values_of("financial documents")
+            .get_many::<String>("financial documents")
             .expect_and_log("error getting brokarage statements pdfs names");
         let pdfnames: Vec<String> = pdfnames.map(|x| x.to_string()).collect();
 
@@ -376,10 +372,10 @@ mod tests {
     #[ignore]
     fn test_sold_dividends_interests_taxation() -> Result<(), clap::Error> {
         // Get all brokerage with dividends only
-        let myapp = App::new("E-trade tax helper").setting(AppSettings::ArgRequiredElseHelp);
+        let myapp = Command::new("etradeTaxHelper").arg_required_else_help(true);
         let rd: Box<dyn etradeTaxReturnHelper::Residency> = Box::new(pl::PL {});
 
-        let matches = create_cmd_line_pattern(myapp).get_matches_from_safe(vec![
+        let matches = create_cmd_line_pattern(myapp).get_matches_from(vec![
             "mytest",
             "etrade_data_2023/Brokerage Statement - XXXXX6557 - 202302.pdf",
             "etrade_data_2023/Brokerage Statement - XXXXX6557 - 202303.pdf",
@@ -390,9 +386,9 @@ mod tests {
             "etrade_data_2023/MS_ClientStatements_6557_202311.pdf",
             "etrade_data_2023/MS_ClientStatements_6557_202312.pdf",
             "etrade_data_2023/G&L_Collapsed-2023.xlsx",
-        ])?;
+        ]);
         let pdfnames = matches
-            .values_of("financial documents")
+            .get_many::<String>("financial documents")
             .expect_and_log("error getting brokarage statements pdfs names");
         let pdfnames: Vec<String> = pdfnames.map(|x| x.to_string()).collect();
 
@@ -417,15 +413,15 @@ mod tests {
     #[test]
     fn test_revolut_dividends_pln() -> Result<(), clap::Error> {
         // Get all brokerage with dividends only
-        let myapp = App::new("E-trade tax helper").setting(AppSettings::ArgRequiredElseHelp);
+        let myapp = Command::new("etradeTaxHelper").arg_required_else_help(true);
         let rd: Box<dyn etradeTaxReturnHelper::Residency> = Box::new(pl::PL {});
 
-        let matches = create_cmd_line_pattern(myapp).get_matches_from_safe(vec![
+        let matches = create_cmd_line_pattern(myapp).get_matches_from(vec![
             "mytest",
             "revolut_data/trading-pnl-statement_2024-01-01_2024-08-04_pl-pl_8e8783.csv",
-        ])?;
+        ]);
         let pdfnames = matches
-            .values_of("financial documents")
+            .get_many::<String>("financial documents")
             .expect_and_log("error getting brokarage statements pdfs names");
         let pdfnames: Vec<String> = pdfnames.map(|x| x.to_string()).collect();
 
@@ -450,15 +446,15 @@ mod tests {
     #[test]
     fn test_revolut_sold_and_dividends() -> Result<(), clap::Error> {
         // Get all brokerage with dividends only
-        let myapp = App::new("E-trade tax helper").setting(AppSettings::ArgRequiredElseHelp);
+        let myapp = Command::new("etradeTaxHelper").arg_required_else_help(true);
         let rd: Box<dyn etradeTaxReturnHelper::Residency> = Box::new(pl::PL {});
 
-        let matches = create_cmd_line_pattern(myapp).get_matches_from_safe(vec![
+        let matches = create_cmd_line_pattern(myapp).get_matches_from(vec![
             "mytest",
             "revolut_data/trading-pnl-statement_2022-11-01_2024-09-01_pl-pl_e989f4.csv",
-        ])?;
+        ]);
         let pdfnames = matches
-            .values_of("financial documents")
+            .get_many::<String>("financial documents")
             .expect_and_log("error getting brokarage statements pdfs names");
         let pdfnames: Vec<String> = pdfnames.map(|x| x.to_string()).collect();
 
@@ -483,15 +479,15 @@ mod tests {
     #[test]
     fn test_revolut_interests_taxation_pln() -> Result<(), clap::Error> {
         // Get all brokerage with dividends only
-        let myapp = App::new("E-trade tax helper").setting(AppSettings::ArgRequiredElseHelp);
+        let myapp = Command::new("etradeTaxHelper").arg_required_else_help(true);
         let rd: Box<dyn etradeTaxReturnHelper::Residency> = Box::new(pl::PL {});
 
-        let matches = create_cmd_line_pattern(myapp).get_matches_from_safe(vec![
+        let matches = create_cmd_line_pattern(myapp).get_matches_from(vec![
             "mytest",
             "revolut_data/Revolut_30cze2023_27lis2023.csv",
-        ])?;
+        ]);
         let pdfnames = matches
-            .values_of("financial documents")
+            .get_many::<String>("financial documents")
             .expect_and_log("error getting brokarage statements pdfs names");
         let pdfnames: Vec<String> = pdfnames.map(|x| x.to_string()).collect();
 
@@ -517,14 +513,14 @@ mod tests {
     #[ignore]
     fn test_sold_dividends_only_taxation() -> Result<(), clap::Error> {
         // Get all brokerage with dividends only
-        let myapp = App::new("E-trade tax helper").setting(AppSettings::ArgRequiredElseHelp);
+        let myapp = Command::new("etradeTaxHelper").arg_required_else_help(true);
         let rd: Box<dyn etradeTaxReturnHelper::Residency> = Box::new(pl::PL {});
-        let matches = create_cmd_line_pattern(myapp).get_matches_from_safe(vec![
+        let matches = create_cmd_line_pattern(myapp).get_matches_from(vec![
             "mytest",
             "data/Brokerage Statement - XXXX0848 - 202206.pdf",
-        ])?;
+        ]);
         let pdfnames = matches
-            .values_of("financial documents")
+            .get_many::<String>("financial documents")
             .expect_and_log("error getting brokarage statements pdfs names");
         let pdfnames: Vec<String> = pdfnames.map(|x| x.to_string()).collect();
 
@@ -550,12 +546,12 @@ mod tests {
     #[ignore]
     fn test_interest_adjustment_taxation() -> Result<(), clap::Error> {
         // Get all brokerage with dividends only
-        let myapp = App::new("E-trade tax helper").setting(AppSettings::ArgRequiredElseHelp);
+        let myapp = Command::new("etradeTaxHelper").arg_required_else_help(true);
         let rd: Box<dyn etradeTaxReturnHelper::Residency> = Box::new(pl::PL {});
         let matches = create_cmd_line_pattern(myapp)
-            .get_matches_from_safe(vec!["mytest", "data/example-interest-adj.pdf"])?;
+            .get_matches_from(vec!["mytest", "data/example-interest-adj.pdf"]);
         let pdfnames = matches
-            .values_of("financial documents")
+            .get_many::<String>("financial documents")
             .expect_and_log("error getting brokarage statements pdfs names");
         let pdfnames: Vec<String> = pdfnames.map(|x| x.to_string()).collect();
 
